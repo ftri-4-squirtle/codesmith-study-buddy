fdab1fe0411c43152985beb81dd5310136b84150
Duke Lee <leeduke90@gmail.com>
2021-11-15 23:35:43 -0500

    start redux setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
