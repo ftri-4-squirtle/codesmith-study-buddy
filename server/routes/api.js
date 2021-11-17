a53050aae6cb3d45c5ef5a1d75d702d0bb611fe6
Daniel Geiger <daniel.w.geiger@gmail.com>
2021-11-16 22:36:57 -0700

    basic create post endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,17 @@ const apiController = require('../controllers/apiController.js');
 
 const router = express.Router();
 
+// / posts - GET
 router.get('/posts', apiController.getPosts, (req, res) => {
     if(!res.locals.posts) {
         res.status(500).json({message: 'No posts found'});
     }
     res.status(200).json(res.locals.posts);
+})
+
+// /posts - POST
+router.post('/posts', apiController.addPost, (req, res) => {
+    res.sendStatus(200);
 })
 
 module.exports = router;
