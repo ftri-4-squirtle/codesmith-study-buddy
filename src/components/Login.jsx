f83fbf1b6350950b22361473958fdce55b2e6df2
Duke Lee <leeduke90@gmail.com>
2021-11-16 20:44:45 -0500

    start navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,7 +25,6 @@ export default function Login() {
 
 	return (
 		<div className={styles.mainContainer}>
-			<h1>Codesmith Study Buddy</h1>
 			<section className={styles.loginContainer}>
 				<form onSubmit={formik.handleSubmit} className={styles.form}>
 					<div>
